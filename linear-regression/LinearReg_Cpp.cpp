f582e3b93c2495f22276d748b1cd06e8a021c399
freeman <1114509193@qq.com>
2024-08-02 23:17:21 +0800

    hsy

---
+++ linear-regression/LinearReg_Cpp.cpp
@@ -0,0 +1,9 @@
+//
+// Created by FreeMan on 2024/8/2.
+//
+
+#include "LinearReg_Cpp.h"
+
+double LinearReg_Cpp::linreg(double x, double w, double b) {
+    return x*w+b;
+}
---
+++ linear-regression/LinearReg_Cpp.h
@@ -0,0 +1,17 @@
+//
+// Created by FreeMan on 2024/8/2.
+//
+
+#ifndef DEEP_LEARNING_MODEL_LINEARREG_CPP_H
+#define DEEP_LEARNING_MODEL_LINEARREG_CPP_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class LinearReg_Cpp {
+public:
+    double linreg(double x,double w,double b);
+};
+
+
+#endif //DEEP_LEARNING_MODEL_LINEARREG_CPP_H
